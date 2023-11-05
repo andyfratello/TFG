fe5464f8b864b6a18511725a9de37ea1859ee7c6
Andreu Orensanz <andreu.orensanz@estudiantat.upc.edu>
2023-11-05 21:29:11 +0100

    Delete p_ccmaqs directory

--- p_ccmaqs/consulta a xe.sql
+++
@@ -1,6 +0,0 @@
-/*SELECT id_sesion, numero_simulacion, codigo_org_planta, orden_de_fabricacion, secuencia, fase, linea, codigo_articulo, tipo, fecha_ini, fecha_fin, recurso, desc_recurso, porc_completado
-    FROM TABLE(pk_web_p_ccmaqs_xe.get_ofs_graph('1', '01', '03', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL));
-*/
-
-SELECT orden_de_fabricacion, secuencia, codigo_articulo, recurso, desc_recurso
-  FROM TABLE(pk_web_p_ccmaqs_xe.get_recursos_graph('1', '01', '03', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL));
